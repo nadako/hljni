0fe018385e2d083696de4686942ab8e868e1519e
Dan Korostelev <nadako@gmail.com>
2018-02-03 23:27:05 +0300

    oops, fix argsAlloc

--- gen/hljni_prelude.c
+++ gen/hljni_prelude.c
@@ -11,7 +11,7 @@ HL_PRIM void HL_NAME(set_env)(JNIEnv* p_env) {
 #define _JNIARGS _ABSTRACT(jvalue)
 #define _JOBJECT _ABSTRACT(_jobject)
 
-HL_PRIM jvalue* HL_NAME(args_alloc)(JNIEnv* p_env, int count) {
+HL_PRIM jvalue* HL_NAME(args_alloc)(int count) {
 	return hl_gc_alloc_noptr(sizeof(jvalue) * count);
 }
 DEFINE_PRIM(_JNIARGS, args_alloc, _I32);
